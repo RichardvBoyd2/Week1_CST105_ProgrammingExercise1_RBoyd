f1e80f879952c2fa04bbf956557ba51275b9f5e1
RichardvBoyd2 <34223701+RichardvBoyd2@users.noreply.github.com>
2017-12-03 19:43:16 -0700

    Prints three lines of info

---
+++ Introduction.java
@@ -0,0 +1,14 @@
+/** Program: Introduction
+ * File: Introduction.java
+ * Summary: Prints three lines of introductory facts about the author
+ * Author: Richard Boyd
+ * Date: December 3rd, 2017
+ */
+
+public class Introduction {
+	public static void main(String[] args) {
+		System.out.println("My name is Richard Boyd"); //these three lines print the facts to the console on three different lines
+		System.out.println("My favorite color is Blue");
+		System.out.println("If I won the lottery, I would buy a house and a Tesla, then invest the rest in cryptocoins");
+	}
+}
