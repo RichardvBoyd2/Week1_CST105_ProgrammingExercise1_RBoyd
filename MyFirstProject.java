8723d26cc80ec96951364252312ced43c41b4ae6
RichardvBoyd2 <34223701+RichardvBoyd2@users.noreply.github.com>
2017-12-11 20:25:11 -0700

    Hopefully done correctly

---
+++ MyFirstProject.java
@@ -0,0 +1,15 @@
+/** Program: Introduction
+ * File: Introduction.java
+ * Summary: Was auto formatted, so it just exists.
+ * Author: Richard Boyd
+ * Date: December 3rd, 2017
+ */
+
+public class Introduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
